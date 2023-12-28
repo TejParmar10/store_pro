049cb736552a9631b4a5076294e397450d3a1c59
Tej Parmar <tejparmar100@gmail.com>
2023-12-29 01:47:50 +0530

    Bottom Navigation BAR done

--- lib/product_store/views/home_view.dart
+++ lib/product_store/views/home_view.dart
@@ -1,12 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:store_pro/app/view/icecream_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Store Pro")),
+    return Scaffold(
+      body: IceCreamView(),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.icecream), label: 'Icecream'),
+          NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+          NavigationDestination(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+        ],
+      ),
     );
   }
 }
